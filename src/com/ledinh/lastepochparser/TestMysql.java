84c4e4eb4a690b7455ef5c78fd139a4f06035db6
lam <lam.ledinh31@gmail.com>
2020-06-04 19:08:19 +0200

    Added mysql connector and test

---
+++ src/com/ledinh/lastepochparser/TestMysql.java
@@ -0,0 +1,21 @@
+package com.ledinh.lastepochparser;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TestMysql {
+    public static void main(String args[]){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con= DriverManager.getConnection("jdbc:mysql://192.168.1.43:3306/LeSuperCoin?useTimezone=true&serverTimezone=GMT%2B8","lam","Azertyuiop123!");
+
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery("select * from emp");
+            while(rs.next())
+                System.out.println(rs.getInt(1));
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
+    }
+}
